cfbd33ae070c2ec17c2401580a28f98dd2adb13c
Alias <heh@kek.com>
2026-04-26 23:47:05 +0800

    Add migration

---
+++ GraphTaskTrackerBackend/Migrations/20260426154645_1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphTaskTrackerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Edges_Nodes_ToNodeId",
+                table: "Edges");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Edges_Nodes_ToNodeId",
+                table: "Edges",
+                column: "ToNodeId",
+                principalTable: "Nodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Edges_Nodes_ToNodeId",
+                table: "Edges");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Edges_Nodes_ToNodeId",
+                table: "Edges",
+                column: "ToNodeId",
+                principalTable: "Nodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
